aaf51449cfc084da26060b5e077efe74f81b8b77
Michiel Borkent <michielborkent@gmail.com>
2020-12-25 16:08:36 +0100

    Add test script

---
+++ test/script.clj
@@ -0,0 +1,13 @@
+#!/usr/bin/env bb
+
+(ns script
+  (:require [babashka.pods :as pods]))
+
+(pods/load-pod "./main")
+
+(require '[pod.babashka.filewatcher :as fw])
+
+(fw/watch "." (fn [event] (prn event)) {:delay-ms 50})
+
+@(promise)
+
