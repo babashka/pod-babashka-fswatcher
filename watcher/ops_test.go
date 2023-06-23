00d1379e3ad505ed02678ffcb8b3ebe43ff3768c
Flávio Sousa <root@fjsousa.com>
2023-06-23 16:01:45 +0100

    capture notifications

--- watcher/ops_test.go
+++ watcher/ops_test.go
@@ -1,7 +1,7 @@
 package watcher
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -14,20 +14,20 @@ import (
 	"github.com/babashka/pod-babashka-fswatcher/babashka"
 )
 
-type FileAction struct {
-	Type string `json:"type"`
-	Path string `json:"path"`
-}
-
 func TestStartWatcher(t *testing.T) {
-	message := babashka.Message{
+
+	watchFolder := "."
+	thisFile := "ops_test.go"
+
+	createMessage := babashka.Message{
 		Op: "invoke", Id: "101", Var: "pod.babashka.fswatcher/-create-watcher", Args: "[101]"}
 
 	startMessage := babashka.Message{
 		Op: "invoke", Id: "2", Var: "pod.babashka.fswatcher/-start-watcher", Args: "[102]"}
 
 	opts := Opts{DelayMs: 50, Recursive: true}
-	watcherInfo, err := createWatcher(&message, ".", opts)
+
+	watcherInfo, err := createWatcher(&createMessage, watchFolder, opts)
 
 	if err != nil {
 		t.Errorf("Error starting watcher: %s", err)
@@ -35,85 +35,82 @@ func TestStartWatcher(t *testing.T) {
 
 	idx := watcherInfo.WatcherId
 
+	if e := startWatcher(&startMessage, idx); e != nil {
+		fmt.Printf("Error starting watcher: %s", e)
+	}
+
+	//touch this file and capture babashka output
+	fsNotifications, err := captureBabashkaOutput(func() error {
+
+		if ee := os.Chtimes(thisFile, time.Now(), time.Now()); ee != nil {
+			return ee
+
+		}
+
+		time.Sleep(100 * time.Millisecond)
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("Error Capturing output: %s", err)
+	}
+
+	fmt.Print("got:", fsNotifications)
+	//"d2:id1:26:statusl4:donee5:value37:{\"type\":\"chmod\",\"path\":\"ops_test.go\"}e"
+	//but structured
+
+}
+
+func captureBabashkaOutput(f func() error) ([]Response, error) {
+
 	defer func(orig *os.File) {
 		os.Stdout = orig
 	}(os.Stdout)
 
 	r, w, _ := os.Pipe()
 	os.Stdout = w
-	e := startWatcher(&startMessage, idx)
-	if e != nil {
-		fmt.Printf("Error starting watcher: %s", e)
+	if err := f(); err != nil {
+		fmt.Print("Failed to touch file and capture output: ", err)
 	}
-
-	//touch this file
-	err = os.Chtimes("ops_test.go", time.Now(), time.Now())
-	if err != nil {
-		t.Fatalf("Failed to touch file: %s", err)
-	}
-
-	time.Sleep(100 * time.Millisecond)
-
-	err = os.Chtimes("ops_test.go", time.Now(), time.Now())
-	if err != nil {
-		t.Fatalf("Failed to touch file: %s", err)
-	}
-
-	time.Sleep(100 * time.Millisecond)
-
 	w.Close()
 	out, _ := io.ReadAll(r)
-	got := string(out)
-	fmt.Println("out:", got)
-	messages := strings.Split(got, "}e")
+
+	outString := string(out)
+
+	bencodeStrings := strings.Split(outString, "}e")
+
+	//return bencodeStrings
+
+	responses := []Response{}
 
 	// Process each bencode message
-	for _, message := range messages {
-		if len(message) == 0 {
+	for _, bencodeString := range bencodeStrings {
+
+		if len(bencodeString) == 0 {
 			continue
 		}
 
-		reader := strings.NewReader(message + "}e")
+		reader := strings.NewReader(bencodeString + "}e")
 
 		// Parse the bencode message into an InvokeResponse struct
-		var response babashka.InvokeResponse
-		err = bencode.Unmarshal(reader, &response)
-		if err != nil {
-			fmt.Printf("Error parsing bencode message: %v\n", err)
-			continue
+		var babashkaMessage babashka.InvokeResponse
+
+		if err := bencode.Unmarshal(reader, &babashkaMessage); err != nil {
+			return nil, err
 		}
 
-		// Process the parsed response as needed
-		fmt.Println("Id:", response)
+		jsonString := babashkaMessage.Value
+
+		var fsnotifyMsg Response
+		// Parse the JSON string into the FileAction struct
+
+		if err := json.Unmarshal([]byte(jsonString), &fsnotifyMsg); err != nil {
+			return nil, err
+		}		
+
+
+		responses = append(responses, fsnotifyMsg)
 	}
 
-	//want := "Hello, World!\n"
-	dataFromFsnotify := babashka.InvokeResponse{}
-	err = bencode.Unmarshal(r, &dataFromFsnotify)
-	if err != nil {
-		t.Fatalf("Failed to touch file: %s", err)
-	}
-
-	//data := whatsthis.(map[string]interface{})["value"]
-	//dataString := data.(string)
-
-	//var action FileAction
-
-	// Parse the JSON string into the FileAction struct
-	//err = json.Unmarshal([]byte(dataString), &action)
-	//if err != nil {
-	//		fmt.Println("Error parsing JSON:", err)
-	//		return
-	//	}
-
-	//	path := action.Path
-	//msg := json.Decoder()
-
-	//if got != want {
-	//	t.Errorf("main() = %v, want %v", got, want)
-	//	}
-
-	//"d2:id1:26:statusl4:donee5:value37:{\"type\":\"chmod\",\"path\":\"ops_test.go\"}e"
-	fmt.Println("out:", dataFromFsnotify)
-
+	return responses, nil
 }
